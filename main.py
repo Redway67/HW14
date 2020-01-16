04595fc40efeaf1fdb1b448c1bdd983b4bfabba8
Igor <redway@mail.ru>
2020-01-16 20:56:42 +0600

    HW14

--- main.py
+++ main.py
@@ -23,18 +23,25 @@ if __name__ == '__main__':
 
     # нужно перевести кирилическое написание города в латинское для запроса
     city = transliteration(city_rus)
+    # какой-то у них на сайте программный косяк с Санкт-Петербургом, остальные города пишутся нормально
+    if city == 'sankt-peterburg':
+        city = 'sankt_peterburg'
+
     url = f'https://pogoda.mail.ru/prognoz/{city}'
-    print(url)
     response = requests.get(url)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        text_temperature = soup.find_all('span', class_="entitem__item-value")
-        max_temperature = text_temperature[0].text.split()
-        min_temperature = text_temperature[1].text.split()
-
-        print(f'В городе {city_rus}  были зарегистрированы:  ')
-        print(f'максимальная температура {max_temperature[0]} в {max_temperature[1]} году')
-        print(f'минимальная {min_temperature[0]} в {min_temperature[1]} году ')
+        temperature = soup.find_all('div', class_ ="information__content__temperature")
+        print(temperature[0].text)
+        arx_temperature = soup.find_all('span', class_="entitem__item-value")
+        if len(arx_temperature) == 4:
+            max_temperature = arx_temperature[0].text.split()
+            min_temperature = arx_temperature[1].text.split()
+            print(f'В городе {city_rus}  были зарегистрированы:  ')
+            print(f'максимальная температура {max_temperature[0]} в {max_temperature[1]} году')
+            print(f'минимальная {min_temperature[0]} в {min_temperature[1]} году ')
+        else:
+            print('Архивных данных нет')
     else:
         print(f'Города {city_rus} нет')
