f7324cea3ed6e10ae6b8c557a7add39368f6f5f9
Igor <redway@mail.ru>
2020-01-19 19:31:28 +0600

    HW15

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import telebot
 from telebot import apihelper
+from parcer import get_temperature
 
 TOKEN = '1014617754:AAHQGgsUEMOZpHs3IZyrUddV4qt10z6HHJI'
 
-proxies = {
-    'http': 'http://167.86.96.4:3128',
-    'https': 'http://167.86.96.4:3128',
-}
+apihelper.proxy = {'https': 'https://85.132.71.82:3128'}
 
-TEXT_START = 'Привет! Это бот сообщающий температуру сейчас в выбранном Вами городе'
-TEXT_HELP = '/t '
-
-apihelper.proxy = proxies
 bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, TEXT_START)
+    bot.reply_to(message, 'Привет! Это бот сообщающий текущую температуру в выбранном Вами городе. \n'
+                          'А также (если есть) архивной температуры,  \n'
+                          'т.е. минимальной и максимальной в этот день в течении последних ста лет \n\n'
+                          'Для справки наберите /help .')
 
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message, TEXT_HELP)
+    bot.reply_to(message, '/t  (Название города) \n'
+                          'Название города для России и стран бывшего СССР можно писать кириллицей. \n'
+                          'Остальные города мира в их правильном написании латиницей. \n '
+                          'По умолчанию будет выбран город Москва')
 
 
-bot.polling()
+@bot.message_handler(commands=['t'])
+def temperature_info(message):
+    city_name = ' '.join(message.text.split(' ')[1:])
+    city_name = city_name.replace(' ', '')
+    answer = get_temperature(city_name)
+    bot.reply_to(message, answer)
+
+
+if __name__ == '__main__':
+    bot.polling()
--- parcer.py
+++ parcer.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 MONTHS = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября',
           'декабря']
 
+url_mail = f'https://pogoda.mail.ru/prognoz/'
+
 
 def transliteration(cyrillic_text):
     cyrillic = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
@@ -17,41 +19,47 @@ def transliteration(cyrillic_text):
     return latin_text
 
 
-if __name__ == '__main__':
-    city_rus = input('Введите название города: ')
-
+def get_temperature(city_rus):
     # по умолчанию Москва
     if len(city_rus) == 0:
         city_rus = 'Москва'
-
     # для запроса нужно перевести кирилическое написание города в латинское
     city = transliteration(city_rus)
     # какой-то у них на сайте программный косяк с Санкт-Петербургом, остальные города пишутся нормально
     if city == 'sankt-peterburg':
         city = 'sankt_peterburg'
 
-    url = f'https://pogoda.mail.ru/prognoz/{city}'
+    url = url_mail + city
     response = requests.get(url)
+
+    print(url)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
 
         temperature = soup.find_all('div', class_="information__content__temperature")
         today_temperature = temperature[0].text.split()[0]
-        print(f'В городе {city_rus} сейчас {today_temperature}')
+        answer = f'В городе {city_rus} сейчас {today_temperature}. \n'
 
         # найдем архивную температуру
         arx_temperature = soup.find_all('span', class_="entitem__item-value")
-        # если архивной температуры нет, то сайт сообщает зачем-то время восхода и заката
+        # если архивной температуры нет, то сайт сообщает зачем-то время восхода и заката, нам это не нужно
         if len(arx_temperature) == 4:
             d = datetime.date.today()
             month = MONTHS[d.month + 1]
             max_temperature = arx_temperature[0].text.split()
             min_temperature = arx_temperature[1].text.split()
-            print(f'Сегодня {d.day} {month}:')
-            print(f'максимальная температура {max_temperature[0]} была в {max_temperature[1]} году')
-            print(f'минимальная {min_temperature[0]} в {min_temperature[1]} году ')
+            answer = answer + f'Сегодня {d.day} {month}:\n'
+            answer = answer + f'максимальная температура {max_temperature[0]} была в {max_temperature[1]} году\n'
+            answer = answer + f'минимальная {min_temperature[0]} в {min_temperature[1]} году\n '
         else:
-            print('Архивных данных по температуре нет')
+            answer = answer + 'Архивных данных по температуре нет'
     else:
-        print(f'Города {city_rus} нет')
+        answer = f'Города {city_rus} нет'
+
+    return answer
+
+
+if __name__ == '__main__':
+    city_cyr = input('Введите название города: ')
+    print(get_temperature(city_cyr))
